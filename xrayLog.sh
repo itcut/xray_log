a3edcf16d36191ae50f19d75049712b2d91ded17
itcut <115387130+itcut@users.noreply.github.com>
2022-11-29 02:50:59 +0800

    Update xrayLog.sh

--- xrayLog.sh
+++ xrayLog.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
  
+wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -O /usr/local/share/xray/geoip.dat
+wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O /usr/local/share/xray/geosite.dat
+ 
 cd /var/log/xray/
 newAccessLog="access.`date +%Y-%m-%d`.log"
 newErrorLog="error.`date +%Y-%m-%d`.log"
@@ -11,6 +14,7 @@ mv error.log $newErrorLog
 touch access.log error.log
 chown nobody:nogroup access.log
 chown nobody:nogroup error.log
+ 
 #reload xray
 systemctl restart xray
  
